799f6bda4a3e5484e4b53d9ab5178596379ca849
Tim Smart <tim.smart@arisechurch.com>
2018-11-08 16:24:52 +1300

    Remove ofx.ts

--- dist/ofx.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ofx_js_1 = require("ofx-js");
-function parseBnz(input) {
-    return ofx_js_1.parse(input);
-}
-exports.parseBnz = parseBnz;
-//# sourceMappingURL=ofx.js.map
--- src/ofx.ts
+++
@@ -1,5 +0,0 @@
-import { parse } from "ofx-js";
-
-export function parseBnz(input: string) {
-  return parse(input);
-}
