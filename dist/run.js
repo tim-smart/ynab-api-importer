f3a8e661a06feb252cba27aa3afb289be821fab8
Tim Smart <tim.smart@arisechurch.com>
2018-11-10 21:10:19 +1300

    Create library

--- dist/run.js
+++ dist/run.js
@@ -3,37 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const logger_js_1 = __importDefault(require("./logger.js"));
-const puppeteer_1 = __importDefault(require("puppeteer"));
-const bnz_js_1 = require("./bnz.js");
-const ynab_js_1 = require("./ynab.js");
+const _1 = __importDefault(require("."));
 const config = require("../config");
-async function main() {
-    // tslint:disable no-console
-    const ynab = new ynab_js_1.YnabWrapperClient(config.ynabAccessToken, config.ynabBudgetID);
-    const browser = await puppeteer_1.default.launch();
-    const bnz = new bnz_js_1.BnzClient({
-        accessNumber: config.bnzAccessNumber,
-        browser,
-        password: config.bnzPassword
-    });
-    logger_js_1.default.info("Logging into BNZ");
-    const bnzDashboard = await bnz.login();
-    await Promise.all(Object.keys(config.accounts)
-        .filter(name => !!name)
-        .map(async (accountName) => {
-        logger_js_1.default.info(`Exporting ${accountName}`);
-        const ynabAccountID = config.accounts[accountName];
-        const ofx = await bnzDashboard.exportAccount(accountName);
-        if (!ofx) {
-            return;
-        }
-        logger_js_1.default.info(`Importing ${accountName}`);
-        await ynab.importOFX(ynabAccountID, ofx);
-    }));
-    await browser.close();
-}
-main().catch(err => {
+_1.default({
+    accounts: config.accounts,
+    bnzAccessNumber: config.bnzAccessNumber,
+    bnzPassword: config.bnzPassword,
+    ynabAccessToken: config.ynabAccessToken,
+    ynabBudgetID: config.ynabBudgetID
+}).catch(err => {
     // tslint:disable no-console
     console.error(err);
     process.exit();
--- src/run.ts
+++ src/run.ts
@@ -1,45 +1,14 @@
-import logger from "./logger.js";
-import puppeteer from "puppeteer";
-import { BnzClient } from "./bnz.js";
-import { YnabWrapperClient } from "./ynab.js";
+import bnzYnabImport from ".";
+
 const config: any = require("../config");
 
-async function main() {
-  // tslint:disable no-console
-  const ynab = new YnabWrapperClient(
-    config.ynabAccessToken,
-    config.ynabBudgetID
-  );
-  const browser = await puppeteer.launch();
-  const bnz = new BnzClient({
-    accessNumber: config.bnzAccessNumber,
-    browser,
-    password: config.bnzPassword
-  });
-
-  logger.info("Logging into BNZ");
-  const bnzDashboard = await bnz.login();
-
-  await Promise.all(
-    Object.keys(config.accounts)
-      .filter(name => !!name)
-      .map(async accountName => {
-        logger.info(`Exporting ${accountName}`);
-        const ynabAccountID = config.accounts[accountName] as string;
-        const ofx = await bnzDashboard.exportAccount(accountName);
-        if (!ofx) {
-          return;
-        }
-
-        logger.info(`Importing ${accountName}`);
-        await ynab.importOFX(ynabAccountID, ofx);
-      })
-  );
-
-  await browser.close();
-}
-
-main().catch(err => {
+bnzYnabImport({
+  accounts: config.accounts,
+  bnzAccessNumber: config.bnzAccessNumber,
+  bnzPassword: config.bnzPassword,
+  ynabAccessToken: config.ynabAccessToken,
+  ynabBudgetID: config.ynabBudgetID
+}).catch(err => {
   // tslint:disable no-console
   console.error(err);
   process.exit();
