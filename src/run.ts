aab401387b63f4fbaadbd120602f874817ba392e
Tim Smart <tim.smart@arisechurch.com>
2020-12-14 13:09:11 +1300

    Move run.ts to bin folder

--- src/run.ts
+++
@@ -1,9 +0,0 @@
-import ynabAPIImporter from ".";
-
-const config: any = require("../config");
-
-ynabAPIImporter(config).catch(err => {
-  // tslint:disable no-console
-  console.error(err.stack);
-  process.exit();
-});
